ceba273ccf93e284666db7bf1f3e8eb5483443cb
Swapnil Alaskar <alaskarswapnil1998@gmail.com>
2026-01-07 21:11:05 +0530

    add new test02 method

--- src/test/java/com/tka/excel_read/TestDemo.java
+++ src/test/java/com/tka/excel_read/TestDemo.java
@@ -6,6 +6,11 @@ public class TestDemo {
 
 	@Test
 	public void test01() {
-		System.out.println("hello");
+		System.out.println("hello1");
+	}
+	
+	@Test
+	public void test02() {
+		System.out.println("hello2");
 	}
 }
