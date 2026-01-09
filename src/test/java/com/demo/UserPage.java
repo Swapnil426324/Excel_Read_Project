91f78ddf5c993b76dfc3952c5d8bfb9f5fe737b6
Swapnil Alaskar <alaskarswapnil1998@gmail.com>
2026-01-09 14:41:26 +0530

    new test case add verify table

---
+++ src/test/java/com/demo/UserPage.java
@@ -0,0 +1,34 @@
+package com.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class UserPage {
+
+	@Test
+	public void validateTble() {
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/SwapnilAlaskar/Documents/Software/Selenium%20Software/Offline%20Website/pages/examples/users.html");
+		List<WebElement> rows = driver.findElements(By.tagName("tr"));
+		List<WebElement> actualData;
+		List<String> actualText = new ArrayList<String>();
+		for (WebElement row : rows) {
+			if (rows.indexOf(row) == 0) {
+				actualData = row.findElements(By.tagName("th"));
+			} else {
+				actualData = row.findElements(By.tagName("td"));
+			}
+
+			for (WebElement dataText : actualData) {
+				System.out.println(dataText.getText());
+			}
+		}
+
+	}
+}
